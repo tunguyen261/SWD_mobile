5aed66af29b11f8df3b8af9826a8e85b6c3a1994
tunguyen261 <nguyendoantu2610@gmail.com>
2023-02-19 21:37:09 +0700

    add to page

--- lib/pages/service_page.dart
+++ lib/pages/service_page.dart
@@ -184,7 +184,26 @@ class LoginScreen2 extends StatelessWidget {
               new Expanded(
                 child: Divider(),
               ),
-
+              new Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0, bottom: 20.0),
+                alignment: Alignment.center,
+                child: new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: new MaterialButton(
+                        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                        color: Colors.transparent,
+                        onPressed: () => {},
+                        child: Text(
+                          "Don't have an account? Create One",
+                          style: TextStyle(color: this.foregroundColor!.withOpacity(0.5)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
